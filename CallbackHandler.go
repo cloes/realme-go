3bc1a784c3f0a7965a70697257fe5f9860aae681
cloes <wayne511200295@163.com>
2018-02-03 12:22:58 +0800

    add feature:添加测试数据

--- CallbackHandler.go
+++ CallbackHandler.go
@@ -5,6 +5,10 @@ import (
 	"encoding/base64"
 	"encoding/xml"
 	"io/ioutil"
+	"encoding/pem"
+	"crypto/x509"
+	"crypto/rsa"
+	"crypto/rand"
 )
 
 //接收返回的xml
@@ -87,11 +91,11 @@ func getResponseContent() ResponseContent{
 
 	resposePrivateKey := v.EncryptedAssertion.EncryptedData.KeyInfo.EncryptedKey.CipherData.CipherValue.Value
 	BaseDecodedResposePrivateKey,_ := base64.StdEncoding.DecodeString(resposePrivateKey)
-	fmt.Println(resposePrivateKey)
+	fmt.Println(BaseDecodedResposePrivateKey)
 
 	resposeEncryptedContent := v.EncryptedAssertion.EncryptedData.CipherData.CipherValue.Value
 	BaseDecodedresposeEncryptedContent,_ := base64.StdEncoding.DecodeString(resposeEncryptedContent)
-	fmt.Println(resposeEncryptedContent)
+	fmt.Println(BaseDecodedresposeEncryptedContent)
 
 	responseContent := ResponseContent{
 		responseStatusCode:resposeStatusCode,
@@ -102,7 +106,41 @@ func getResponseContent() ResponseContent{
 	return responseContent
 }
 
+
+func getAESKEY()([]byte, error){
+	privateKey,err := ioutil.ReadFile("private_key.txt")
+	if err != nil {
+		fmt.Printf("Something went wrong: %s", err)
+	}
+	block, _ := pem.Decode(privateKey)
+	if block == nil {
+		//return nil, err.New("private key error!")
+		fmt.Println("Something went wrong")
+	}
+
+	fmt.Println(block.Type)
+	fmt.Println(block.Headers)
+
+	priv, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		fmt.Printf("Something went wrong: %s", err)
+	}
+
+	data,err := ioutil.ReadFile("aes_key.txt")
+	data2,_ := base64.StdEncoding.DecodeString(string(data))
+	if err != nil {
+		fmt.Printf("Something went wrong: %s", err)
+	}
+	return rsa.DecryptPKCS1v15(rand.Reader, priv.(*rsa.PrivateKey), data2)
+
+}
+
 func main(){
-	getResponseContent()
+	//responseContent := getResponseContent()
+	tmp,err := getAESKEY()
+	if err != nil {
+		fmt.Printf("Something went wrong: %s", err)
+	}
+	fmt.Println(string(tmp))
 
 }
